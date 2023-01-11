f8e9d7bf4a4defdbbceaba64e4c6353dac4aa00e
Estuardo Aguirre <EstuardoA25@gmail.com>
2023-01-11 16:42:49 -0600

    chore(i18n): :heavy_plus_sign: add i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@nuxtjs/i18n',
     '@unocss/nuxt',
   ],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [
+      {
+        code: 'en',
+        file: 'en-US.yaml',
+        name: 'English',
+      },
+      {
+        code: 'es',
+        file: 'es-ES.yaml',
+        name: 'Español',
+      },
+    ],
+    lazy: true,
+    langDir: 'lang',
+    vueI18n: {
+      legacy: false,
+    },
+  },
   typescript: {
     shim: false,
   },
