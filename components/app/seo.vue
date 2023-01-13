75a870d9cc21579e57ce7e7e20a524b13838aba1
Estuardo Aguirre <EstuardoA25@gmail.com>
2023-01-13 11:25:13 -0600

    chore(seo): :mag: add favicon variants

--- components/app/seo.vue
+++ components/app/seo.vue
@@ -12,6 +12,9 @@ useHead({
 
 <template>
   <Html :lang="head.htmlAttrs?.lang" :dir="head.htmlAttrs?.dir">
+    <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     <template v-for="link in head.link" :key="link.id">
       <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
     </template>
